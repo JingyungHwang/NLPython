4d77688ed592b167a95ca9e710af8e5e7c4d4bd2
JingyungHwang <godnamu1213@gmail.com>
2025-05-28 03:03:54 -0700

    교재의 토큰화 내용.

---
+++ 코드보관소/교재토큰화.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+
+def wordtokenization():
+    content = """Stemming is funnier than a bummer says the sushi loving computer scientist.
+    She really wants to buy cars. She told me argraily. It is better for you.
+    Man is walking. We are meeting tomorrow. You really don't know..!"""
+    print (word_tokenize(content))
+
+def wordlemmatization():
+    wordlemma = WordNetLemmatizer
+    print(wordlemma.lemmatize('cars'))
+    print (wordlemma.lemmatize('walking', pos = 'v'))
+    print (wordlemma . lemmatize('meeting', pos = 'n'))
+    print(wordlemma.lemmatize(), 'meeting', pos = 'v')
+    print(wordlemma.lemmatize('better', pos = 'a'))
+    print(wordlemma.lemmatize('is', pos ='v'))
+    print(wordlemma.lemmatize('funnier', pos = 'a'))
+    print(wordlemma.lemmatize('expected', pos = 'v'))
+    print(wordlemma.lemmatize('funtasized', pos = 'v'))
+
+if __name__ == "__main__":
+    wordtokenization()
+    print("\n")
+    print("---------------------word Lemmatizqation---------------------")
+    wordlemmatization()
